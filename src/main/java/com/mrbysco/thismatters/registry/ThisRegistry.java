df0c0cdd031bccc982f36fe782f27ebe5edd0fa6
Mrbysco <Mrbysco@users.noreply.github.com>
2023-06-27 22:11:34 +0200

    Fix creative tab ordering [build] [publish]

--- src/main/java/com/mrbysco/thismatters/registry/ThisRegistry.java
+++ src/main/java/com/mrbysco/thismatters/registry/ThisRegistry.java
@@ -7,6 +7,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
@@ -37,6 +38,7 @@ public class ThisRegistry {
 
 	public static final RegistryObject<CreativeModeTab> MATTER_TAB = CREATIVE_MODE_TABS.register("tab", () -> CreativeModeTab.builder()
 			.icon(() -> new ItemStack(ThisRegistry.ORGANIC_MATTER_COMPRESSOR.get()))
+			.withTabsBefore(CreativeModeTabs.SPAWN_EGGS)
 			.title(Component.translatable("itemGroup.thismatters"))
 			.displayItems((displayParameters, output) -> {
 				List<ItemStack> stacks = ThisRegistry.ITEMS.getEntries().stream().map(reg -> new ItemStack(reg.get())).toList();
